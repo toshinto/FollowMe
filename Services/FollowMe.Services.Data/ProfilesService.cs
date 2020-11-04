346549c25119884ae76f3f64bf80306b6632f939
Todor Georgiev <lyvcheto@abv.bg>
2020-11-04 14:29:38 +0200

    Add Details to navbar menu

--- Services/FollowMe.Services.Data/ProfilesService.cs
+++ Services/FollowMe.Services.Data/ProfilesService.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+
+using FollowMe.Data.Common.Repositories;
+using FollowMe.Data.Models;
+using FollowMe.Services.Mapping;
 
 namespace FollowMe.Services.Data
 {
     public class ProfilesService : IProfilesService
     {
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+
+        public ProfilesService(IDeletableEntityRepository<ApplicationUser> usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+        public string GetId(string id)
+        {
+            var userId = this.usersRepository.All().Where(x => x.Id == id).Select(x => x.Id).FirstOrDefault();
+
+            return userId;
+        }
     }
 }
--- Web/FollowMe.Web/Controllers/ProfilesController.cs
+++ Web/FollowMe.Web/Controllers/ProfilesController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FollowMe.Services.Data;
+using FollowMe.Web.ViewModels.Profiles;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FollowMe.Web.Controllers
 {
-    public class ProfilesController : Controller
+    public class ProfilesController : BaseController
     {
-        public IActionResult Details()
+        private readonly IProfilesService profilesService;
+
+        public ProfilesController(IProfilesService profilesService)
         {
+            this.profilesService = profilesService;
+        }
+        public IActionResult Details(string id)
+        {
+            var viewModel = new ProfileIdViewModel
+            {
+                Id = this.profilesService.GetId(id),
+            };
             return this.View();
         }
     }
