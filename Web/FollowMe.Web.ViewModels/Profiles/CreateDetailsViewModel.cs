43a155efad8b5d24b5125b57a6075b679d08493c
Todor Georgiev <lyvcheto@abv.bg>
2020-11-05 11:26:28 +0200

    Work on details page

---
+++ Web/FollowMe.Web.ViewModels/Profiles/CreateDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FollowMe.Web.ViewModels.Profiles
+{
+    public class CreateDetailsViewModel
+    {
+        public Enum EyeColor { get; set; }
+    }
+}
