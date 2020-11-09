bd05bc496bfdc28b4f3a8bec5fae9cb1b97cca18
Todor Georgiev <lyvcheto@abv.bg>
2020-11-09 14:44:40 +0200

    Add PostController and services for it

---
+++ Web/FollowMe.Web/Controllers/PostsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FollowMe.Web.Controllers
+{
+    public class PostsController
+    {
+    }
+}
