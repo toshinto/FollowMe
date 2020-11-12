58dda2c794d679352627373b857d8a9ddb3a9645
Todor Georgiev <lyvcheto@abv.bg>
2020-11-12 12:14:16 +0200

    Add comment db set to application db context

---
+++ Data/FollowMe.Data/Migrations/20201112101320_AddDbSetComment.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FollowMe.Data.Migrations
+{
+    public partial class AddDbSetComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Posts_PostId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_UserId",
+                table: "Comments",
+                newName: "IX_Comments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_PostId",
+                table: "Comments",
+                newName: "IX_Comments_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_IsDeleted",
+                table: "Comments",
+                newName: "IX_Comments_IsDeleted");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_UserId",
+                table: "Comment",
+                newName: "IX_Comment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_PostId",
+                table: "Comment",
+                newName: "IX_Comment_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_IsDeleted",
+                table: "Comment",
+                newName: "IX_Comment_IsDeleted");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Posts_PostId",
+                table: "Comment",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
