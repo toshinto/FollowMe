d2382d9374cfef15b5a339e6ee00e3b559bab24f
Todor Georgiev <lyvcheto@abv.bg>
2020-11-13 11:44:05 +0200

    Implement vote services

--- Services/FollowMe.Services.Data/IVotesService.cs
+++ Services/FollowMe.Services.Data/IVotesService.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FollowMe.Services.Data
 {
     public interface IVotesService
     {
+        Task VoteAsync(string postId, string userId, bool isUpVote);
+
+        string GetVotes(string postId);
     }
 }
--- Services/FollowMe.Services.Data/VotesService.cs
+++ Services/FollowMe.Services.Data/VotesService.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using FollowMe.Data.Common.Repositories;
+using FollowMe.Data.Models;
 
 namespace FollowMe.Services.Data
 {
     public class VotesService : IVotesService
     {
+        private readonly IRepository<Vote> votesRepository;
+
+        public VotesService(IRepository<Vote> votesRepository)
+        {
+            this.votesRepository = votesRepository;
+        }
+
+        public int GetVotes(string postId)
+        {
+            var votes = this.votesRepository.All()
+                .Where(x => x.PostId == postId).Sum(x => (int)x.Type);
+            return votes;
+        }
+
+        public async Task VoteAsync(string postId, string userId, bool isUpVote)
+        {
+            var vote = this.votesRepository.All()
+                .FirstOrDefault(x => x.PostId == postId && x.UserId == userId);
+            if (vote != null)
+            {
+                vote.Type = isUpVote ? VoteType.UpVote : VoteType.DownVote;
+            }
+            else
+            {
+                vote = new Vote
+                {
+                    PostId = postId,
+                    UserId = userId,
+                    Type = isUpVote ? VoteType.UpVote : VoteType.DownVote,
+                };
+
+                await this.votesRepository.AddAsync(vote);
+            }
+
+            await this.votesRepository.SaveChangesAsync();
+        }
     }
 }
