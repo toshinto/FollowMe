64fd51951cec5e119def1cdf2fe6477c3ab495eb
Todor Georgiev <lyvcheto@abv.bg>
2020-11-21 14:43:58 +0200

    Add EditDetailsViewModel

---
+++ Web/FollowMe.Web.ViewModels/Profiles/EditDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FollowMe.Data.Models;
+using FollowMe.Services.Mapping;
+
+namespace FollowMe.Web.ViewModels.Profiles
+{
+    public class EditDetailsViewModel : IMapFrom<UserCharacteristic>
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Description { get; set; }
+
+        public string FullName => $"{this.FirstName} {this.LastName}";
+    }
+}
