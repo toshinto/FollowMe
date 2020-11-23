db417fa74f23cbef5593829ac55e0bd778718198
Todor Georgiev <lyvcheto@abv.bg>
2020-11-23 13:03:18 +0200

    Add validation attributes on create and edit post

--- Web/FollowMe.Web.ViewModels/Posts/PostsCreateModel.cs
+++ Web/FollowMe.Web.ViewModels/Posts/PostsCreateModel.cs
@@ -1,4 +1,6 @@
-﻿using FollowMe.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+using FollowMe.Data.Models;
 using FollowMe.Services.Mapping;
 
 namespace FollowMe.Web.ViewModels.Posts
@@ -7,9 +9,17 @@ namespace FollowMe.Web.ViewModels.Posts
     {
         public string UserUserName { get; set; }
 
+        [Required]
         public string UserId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Content { get; set; }
     }
 }
--- Web/FollowMe.Web.ViewModels/Profiles/EditPostViewModel.cs
+++ Web/FollowMe.Web.ViewModels/Profiles/EditPostViewModel.cs
@@ -1,4 +1,6 @@
-﻿using FollowMe.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+using FollowMe.Data.Models;
 using FollowMe.Services.Mapping;
 
 namespace FollowMe.Web.ViewModels.Profiles
@@ -7,10 +9,17 @@ namespace FollowMe.Web.ViewModels.Profiles
     {
         public string Id { get; set; }
 
+        [Required]
         public string PostId { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Content { get; set; }
     }
 }
