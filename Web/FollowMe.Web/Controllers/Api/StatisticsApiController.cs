6381d7660401f5c2841b2a06447a5bbb250892c7
Todor Georgiev <lyvcheto@abv.bg>
2022-03-16 18:03:32 +0200

    Added WebApi call from db return json.

---
+++ Web/FollowMe.Web/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,30 @@
+﻿using FollowMe.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FollowMe.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext data;
+
+        public StatisticsApiController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Test()
+        {
+            var userId = this.data
+                .Users
+                .Select(t => t.Id)
+                .ToArray();
+
+            return userId;
+        }
+    }
+}
